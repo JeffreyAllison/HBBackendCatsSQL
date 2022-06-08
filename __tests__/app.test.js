50692707df13ca4ab022033f0d998b379acb5868
JeffAllison <itsjallison@gmail.com>
2022-06-08 15:15:47 -0700

    refactored tests in app.test.js, refactored getters in cats.js controller file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const { cats } = require('../data/cats');
+const Cat = require('../lib/models/Cat');
 
 describe('cats routes', () => {
   beforeEach(() => {
@@ -11,6 +11,7 @@ describe('cats routes', () => {
 
   it('/cats should return a list of cats', async () => {
     const res = await request(app).get('/cats');
+    const cats = await Cat.getAll();
     const expected = cats.map((cat) => {
       return { id: cat.id, name: cat.name };
     });
@@ -20,13 +21,13 @@ describe('cats routes', () => {
   it('/cats/:id should return cat detail', async () => {
     const res = await request(app).get('/cats/1');
     const felix = {
-      id: '1',
+      id: 1,
       name: 'Felix',
       type: 'Tuxedo',
       url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/200px-Felix_the_cat.svg.png',
       year: 1892,
       lives: 3,
-      isSidekick: false,
+      issidekick: false,
     };
     expect(res.body).toEqual(felix);
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -4,16 +4,16 @@
 DROP table if exists cats; 
 
 CREATE table cats (
-  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  id INT GENERATED ALWAYS AS IDENTITY,
   name VARCHAR NOT NULL,
   type VARCHAR NOT NULL, 
   url VARCHAR NOT NULL,
   year INT NOT NULL,
   lives INT NOT NULL,
-  isSidekick BOOLEAN
+  issidekick BOOLEAN
 );
 
-INSERT INTO cats (name, type, url, year, lives, isSidekick) VALUES
+INSERT INTO cats (name, type, url, year, lives, issidekick) VALUES
 ('Felix','Tuxedo','https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/200px-Felix_the_cat.svg.png',1892,3,false),
 
 ('Garfield','Orange Tabby','https://static.wikia.nocookie.net/garfield/images/9/9f/GarfieldCharacter.jpg',1978,7,false),
